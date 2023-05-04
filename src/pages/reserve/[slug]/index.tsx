d244f28ccd4158c33b9282b0240c60d690fcd6b2
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 15:39:07 -0400

    Home page link

--- src/pages/reserve/[slug]/index.tsx
+++ src/pages/reserve/[slug]/index.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
+
 export default function ReserveRestaurant() {
     return (
 
@@ -5,9 +8,9 @@ export default function ReserveRestaurant() {
             <main className="max-w-screen-2xl m-auto bg-white">
                 {/* NAVBAR */}
                 <nav className="bg-white p-2 flex justify-between">
-                    <a href="" className="font-bold text-gray-700 text-2xl">
+                    <Link href="/" className="font-bold text-gray-700 text-2xl">
                         {" "} OpenTable{" "}
-                    </a>
+                    </Link>
                     <div>
                         <div className="flex">
                             <button
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Search() {
     return (
 
@@ -5,9 +7,9 @@ export default function Search() {
             <main className="max-w-screen-2xl m-auto bg-white">
                 {/* NAVBAR */}
                 <nav className="bg-white p-2 flex justify-between">
-                    <a href="" className="font-bold text-gray-700 text-2xl">
+                    <Link href="/" className="font-bold text-gray-700 text-2xl">
                         {" "} OpenTable{" "}
-                    </a>
+                    </Link>
                     <div>
                         <div className="flex">
                             <button

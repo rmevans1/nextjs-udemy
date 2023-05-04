5ccf5fb5ace0102174f4d3cc52496ff83a9245fd
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 16:58:02 -0400

    Reservation header component

--- src/pages/reserve/[slug]/index.tsx
+++ src/pages/reserve/[slug]/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import NavBar from "@/components/NavBar";
+import Header from "@/pages/reserve/components/Header";
 
 
 export default function ReserveRestaurant() {
@@ -10,28 +11,8 @@ export default function ReserveRestaurant() {
                 <NavBar />
                 <div className="border-t h-screen">
                     <div className="py-9 w-3/5 m-auto">
-                        {/* HEADER */}
-                        <div>
-                            <h3 className="font-bold">You're almost done!</h3>
-                            <div className="mt-5 flex">
-                                <img
-                                    src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-                                    alt=""
-                                    className="w-32 h-18 rounded"
-                                />
-                                <div className="ml-4">
-                                    <h1 className="text-3xl font-bold">
-                                        Aiāna Restaurant Collective
-                                    </h1>
-                                    <div className="flex mt-3">
-                                        <p className="mr-6">Tues, 22, 2023</p>
-                                        <p className="mr-6">7:30 PM</p>
-                                        <p className="mr-6">3 people</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* HEADER */} {/* FORM */}
+                        <Header />
+                        {/* FORM */}
                         <div className="mt-10 flex flex-wrap justify-between w-[660px]">
                             <input
                                 type="text"

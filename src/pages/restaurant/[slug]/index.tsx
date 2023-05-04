b6f04cb64400ffa20b64fd01f4c7d940d4f028b8
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 15:50:33 -0400

    Menu/Overview links

--- src/pages/restaurant/[slug]/index.tsx
+++ src/pages/restaurant/[slug]/index.tsx
@@ -36,8 +36,8 @@ export default function RestaurantDetails() {
                     <div className="bg-white w-[70%] rounded p-3 shadow">
                         {/* RESAURANT NAVBAR */}
                         <nav className="flex text-reg border-b pb-2">
-                            <a href="" className="mr-7"> Overview </a>
-                            <a href="" className="mr-7"> Menu </a>
+                            <Link href="/restaurant/test-restaurant" className="mr-7"> Overview </Link>
+                            <Link href="/restaurant/test-restaurant/menu" className="mr-7"> Menu </Link>
                         </nav>
                         {/* RESAURANT NAVBAR */} {/* TITLE */}
                         <div className="mt-4 border-b pb-6">

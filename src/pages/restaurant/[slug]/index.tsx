c81c5aeb9d0e3681aa054803366abc7c2399b1a5
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 16:29:30 -0400

    Restaurant header

--- src/pages/restaurant/[slug]/index.tsx
+++ src/pages/restaurant/[slug]/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import NavBar from "@/components/NavBar";
+import Header from "@/pages/restaurant/components/Header";
 
 export default function RestaurantDetails() {
     return (
@@ -7,17 +8,8 @@ export default function RestaurantDetails() {
         <main className="bg-gray-100 min-h-screen w-screen">
             <main className="max-w-screen-2xl m-auto bg-white">
                 <NavBar />
-                {/* HEADER */}
-                <div className="h-96 overflow-hidden">
-                    <div
-                        className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center"
-                    >
-                        <h1 className="text-7xl text-white captitalize text-shadow text-center">
-                            Milestones Grill (Toronto)
-                        </h1>
-                    </div>
-                </div>
-                {/* HEADER */} {/* DESCRIPTION PORTION */}
+                <Header />
+                {/* DESCRIPTION PORTION */}
                 <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
                     <div className="bg-white w-[70%] rounded p-3 shadow">
                         {/* RESAURANT NAVBAR */}

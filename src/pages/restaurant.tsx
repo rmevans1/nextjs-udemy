1f001a8715801b2c5c3c42bfc4aef80bfe3017b8
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 14:21:38 -0400

    Title and Description

--- src/pages/restaurant.tsx
+++ src/pages/restaurant.tsx
@@ -35,6 +35,41 @@ export default function Home() {
                         </div>
                     </div>
                 {/* HEADER */}
+                {/* DESCRIPTION PORTION*/}
+                <div className={'flex m-auto w-2/3 justify-between items-start 0 -mt-11'}>
+                    <div className={'bg-white w-[70%] rounded p-3 shadow'}>
+                        {/* Restaurant Nav */}
+                            <nav className={'flex text-reg border-b pb-2'}>
+                                <a href={''} className={'mr-7'}>Overview</a>
+                                <a href={''} className={'mr-7'}>Menu</a>
+                            </nav>
+                        {/* Restaurant Nav */}
+                        {/* Title */}
+                        <div className={'mt-4 border-b pb-6'}>
+                            <h1 className={'font-bold text-6xl'}>Milestones Grill</h1>
+                        </div>
+                        {/* Title */}
+                        {/* Rating */}
+                        <div className={'flex items-end'}>
+                            <div className={'ratings mt-2 flex items-center'}>
+                                <p>*****</p>
+                                <p className={'text-reg ml-3'}>4.9</p>
+                            </div>
+                            <div>
+                                <p className={'text-reg ml-4'}>600 Reviews</p>
+                            </div>
+                        </div>
+                        {/* Rating */}
+                        {/* Description */}
+                        <div className={'mt-4'}>
+                            <p className={'text-lg font-light'}>
+                                Welcome to Greg Norman Australian Grille – an Australian heritage restaurant on the waterfront at Barefoot Landing in Myrtle Beach, SC. Our innovative menu features exotic Australian fare and a wine list that has received the Wine Spectator Award of Excellence for more than 15 years, consistently ranking us as Myrtle Beach’s premier steak and seafood restaurant. We didn’t invent waterfront dining. We only mastered it.
+                            </p>
+                        </div>
+                        {/* Description */}
+                    </div>
+                </div>
+                {/* DESCRIPTION PORTION*/}
             </main>
         </main>
     );

5411885ca6bed752e261c7d8bc3bd00245a4a1bc
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 16:33:34 -0400

    Restaurant Nav

--- src/pages/restaurant/[slug]/index.tsx
+++ src/pages/restaurant/[slug]/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import NavBar from "@/components/NavBar";
 import Header from "@/pages/restaurant/components/Header";
+import RestaurantNav from "@/pages/restaurant/components/RestaurantNav";
 
 export default function RestaurantDetails() {
     return (
@@ -12,12 +13,8 @@ export default function RestaurantDetails() {
                 {/* DESCRIPTION PORTION */}
                 <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
                     <div className="bg-white w-[70%] rounded p-3 shadow">
-                        {/* RESAURANT NAVBAR */}
-                        <nav className="flex text-reg border-b pb-2">
-                            <Link href="/restaurant/test-restaurant" className="mr-7"> Overview </Link>
-                            <Link href="/restaurant/test-restaurant/menu" className="mr-7"> Menu </Link>
-                        </nav>
-                        {/* RESAURANT NAVBAR */} {/* TITLE */}
+                        <RestaurantNav />
+                        {/* TITLE */}
                         <div className="mt-4 border-b pb-6">
                             <h1 className="font-bold text-6xl">Milesstone Grill</h1>
                         </div>

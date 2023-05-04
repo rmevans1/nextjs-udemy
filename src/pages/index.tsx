cdc99f49d2861bba2112f61d01d5bcc4827b9fdb
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 16:13:07 -0400

    Clean up code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import {Inter} from 'next/font/google'
-import Link from "next/link";
 import NavBar from "@/components/NavBar";
 import Header from "@/components/Header";
 import RestaurantCard from "@/components/RestaurantCard";

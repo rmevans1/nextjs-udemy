1056f131b53e4e89cc3b85dcc0439b677e09c87e
Robert Evans <robertevans@robertevansmb.com>
2023-05-04 16:59:15 -0400

    Form component

--- src/pages/reserve/[slug]/index.tsx
+++ src/pages/reserve/[slug]/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import NavBar from "@/components/NavBar";
 import Header from "@/pages/reserve/components/Header";
+import Form from "@/pages/reserve/components/Form";
 
 
 export default function ReserveRestaurant() {
@@ -12,49 +13,7 @@ export default function ReserveRestaurant() {
                 <div className="border-t h-screen">
                     <div className="py-9 w-3/5 m-auto">
                         <Header />
-                        {/* FORM */}
-                        <div className="mt-10 flex flex-wrap justify-between w-[660px]">
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="First name"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Last name"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Phone number"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Email"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Occasion (optional)"
-                            />
-                            <input
-                                type="text"
-                                className="border rounded p-3 w-80 mb-4"
-                                placeholder="Requests (optional)"
-                            />
-                            <button
-                                className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
-                            >
-                                Complete reservation
-                            </button>
-                            <p className="mt-4 text-sm">
-                                By clicking “Complete reservation” you agree to the OpenTable Terms
-                                of Use and Privacy Policy. Standard text message rates may apply.
-                                You may opt out of receiving text messages at any time.
-                            </p>
-                        </div>
+                        <Form />
                     </div>
                 </div>
             </main>

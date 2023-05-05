fa6206951c85c07484063aa4f7c8088ec00871d3
Robert Evans <robertevans@robertevansmb.com>
2023-05-05 09:36:32 -0400

    Reservation page

--- src/pages/reserve/[slug]/index.tsx
+++ src/pages/reserve/[slug]/index.tsx
@@ -6,18 +6,13 @@ import Form from "@/pages/reserve/components/Form";
 
 export default function ReserveRestaurant() {
     return (
-
-        <main className="bg-gray-100 min-h-screen w-screen">
-            <main className="max-w-screen-2xl m-auto bg-white">
-                <NavBar />
-                <div className="border-t h-screen">
-                    <div className="py-9 w-3/5 m-auto">
-                        <Header />
-                        <Form />
-                    </div>
+        <>
+            <div className="border-t h-screen">
+                <div className="py-9 w-3/5 m-auto">
+                    <Header/>
+                    <Form/>
                 </div>
-            </main>
-        </main>
-
-)
+            </div>
+        </>
+    )
 }

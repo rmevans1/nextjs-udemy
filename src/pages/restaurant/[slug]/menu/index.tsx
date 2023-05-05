147080bd94745672d3cd97b3bea0ba683248a6a0
Robert Evans <robertevans@robertevansmb.com>
2023-05-05 15:06:57 -0400

    Add title to restaurant menu

--- src/pages/restaurant/[slug]/menu/index.tsx
+++ src/pages/restaurant/[slug]/menu/index.tsx
@@ -1,10 +1,14 @@
 import Header from "@/pages/restaurant/components/Header";
 import RestaurantNav from "@/pages/restaurant/components/RestaurantNav";
 import Menu from "@/pages/restaurant/components/Menu";
+import Head from "next/head";
 
 export default function RestaurantMenu() {
     return (
         <>
+            <Head>
+                <title>Menu | Test Restaurant | OpenTable</title>
+            </Head>
             <Header/>
             <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
                 <div className="bg-white w-[100%] rounded p-3 shadow">

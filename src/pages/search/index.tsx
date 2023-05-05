ea48e475155d9a7af4de573f96c82db45bdbcfdc
Robert Evans <robertevans@robertevansmb.com>
2023-05-05 09:34:53 -0400

    search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,19 +5,14 @@ import RestaurantCard from "@/pages/search/components/RestaurantCard";
 
 export default function Search() {
     return (
-
-        <main className="bg-gray-100 min-h-screen w-screen">
-            <main className="max-w-screen-2xl m-auto bg-white">
-                <NavBar />
-                <Header />
-                <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-                    <SideBar />
-                    <div className="w-5/6">
-                        <RestaurantCard />
-                    </div>
+        <>
+            <Header/>
+            <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+                <SideBar/>
+                <div className="w-5/6">
+                    <RestaurantCard/>
                 </div>
-            </main>
-        </main>
-
+            </div>
+        </>
     )
 }

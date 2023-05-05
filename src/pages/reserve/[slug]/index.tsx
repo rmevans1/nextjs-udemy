e4bb01cc898b887a4b082ae76da093b3c165ab5a
Robert Evans <robertevans@robertevansmb.com>
2023-05-05 15:07:23 -0400

    Add title to reservation page

--- src/pages/reserve/[slug]/index.tsx
+++ src/pages/reserve/[slug]/index.tsx
@@ -1,10 +1,14 @@
 import Header from "@/pages/reserve/components/Header";
 import Form from "@/pages/reserve/components/Form";
+import Head from "next/head";
 
 
 export default function ReserveRestaurant() {
     return (
         <>
+            <Head>
+                <title>Make a reservation | Test Restaurant | OpenTable</title>
+            </Head>
             <div className="border-t h-screen">
                 <div className="py-9 w-3/5 m-auto">
                     <Header/>

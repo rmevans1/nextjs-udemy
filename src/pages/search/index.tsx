e47667709e68028b835188c79d5370d92c7db182
Robert Evans <robertevans@robertevansmb.com>
2023-05-05 15:06:06 -0400

    Add title to search results

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,14 @@
 import Header from "@/pages/search/components/Header";
 import SideBar from "@/pages/search/components/SideBar";
 import RestaurantCard from "@/pages/search/components/RestaurantCard";
+import Head from "next/head";
 
 export default function Search() {
     return (
         <>
+            <Head>
+                <title>Search Results | OpenTable</title>
+            </Head>
             <Header/>
             <div className="flex py-4 m-auto w-2/3 justify-between items-start">
                 <SideBar/>

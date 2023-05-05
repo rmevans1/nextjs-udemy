7f5b89294ef05816ddad007d92919af9efed43ff
Robert Evans <robertevans@robertevansmb.com>
2023-05-05 09:36:05 -0400

    Menu page

--- src/pages/restaurant/[slug]/menu/index.tsx
+++ src/pages/restaurant/[slug]/menu/index.tsx
@@ -5,20 +5,15 @@ import Menu from "@/pages/restaurant/components/Menu";
 
 export default function RestaurantMenu() {
     return (
-
-        <main className="bg-gray-100 min-h-screen w-screen">
-            <main className="max-w-screen-2xl m-auto bg-white">
-                <NavBar />
-                <Header />
-                <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-                    <div className="bg-white w-[100%] rounded p-3 shadow">
-                        <RestaurantNav />
-                        <Menu />
-                    </div>
+        <>
+            <Header/>
+            <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+                <div className="bg-white w-[100%] rounded p-3 shadow">
+                    <RestaurantNav/>
+                    <Menu/>
                 </div>
-                {/* DESCRIPTION PORTION */}
-            </main>
-        </main>
-
+            </div>
+            {/* DESCRIPTION PORTION */}
+        </>
     )
 }

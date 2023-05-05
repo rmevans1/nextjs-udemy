b0022eaa48283b23ef5d0827712999a979f8ee49
Robert Evans <robertevans@robertevansmb.com>
2023-05-05 09:33:24 -0400

    Refactor home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
+import NavBar from "@/components/NavBar";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head/>
+            <body>
+            <main className="bg-gray-100 min-h-screen w-screen">
+                <main className="max-w-screen-2xl m-auto bg-white">
+                    <NavBar/>
+                    <Main/>
+
+                </main>
+            </main>
+            <NextScript/>
+            </body>
+        </Html>
+    )
 }
